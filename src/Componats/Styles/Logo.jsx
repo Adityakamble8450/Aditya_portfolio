8f6e17262626902d5e5f43eed89be1225b099103
Aditya kamble <kambleaditya490@gmail.com>
2024-05-14 23:13:19 +0530

    editing logo

--- src/Componats/Styles/Logo.jsx
+++ src/Componats/Styles/Logo.jsx
@@ -6,10 +6,10 @@ const Logo = () => {
 
   const Name = "Aditya";
 
-  Shery.makeMagnet(".magnet-target", {
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+  // Shery.makeMagnet(".magnet-target", {
+  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  //   duration: 1,
+  // });
 
   return (
     <Link to="/">
